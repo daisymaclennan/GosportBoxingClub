8a05de0401d46d77d24229af68fdc83a4afedba3
Daisy Maclennan <daisy@sidigital.co>
2020-02-26 11:04:06 +0000

    Add class CPT

---
+++ wp-content/themes/gosportboxingclub/our-classes.php
@@ -0,0 +1,10 @@
+<?php
+  /*Template Name: Our Classes Template */
+?>
+<?php get_header() ?>
+
+<div class="content">
+  <h1>Our classes</h1>
+  <p class="paragraph"><?= get_field('content') ?></p>
+
+<?php get_footer() ?>
